2e6655524f68a7f851bc504f1eaaedd50a835b0e
Jonathanpatinocs <jonathanpatinocs@gmail.com>
2024-06-08 20:01:38 -0400

    stuff

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './icon-light.png';
 
 function component() {
     const element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-
     element.classList.add('hello');
 
+    const icon_light = new Image();
+    icon_light.src = Icon;
+
+    element.appendChild(icon_light);
     return element;
 }
+console.log("helo");
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        watchFiles: ['./src/index.html'],
+        static: './dist',
+    },
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
@@ -11,6 +20,23 @@ module.exports = {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            
+        }),
+    ],
+    
 };
